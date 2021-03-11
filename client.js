fafc11d763c15f2b2b2b2b7c765405e87ece281e
Sam T <bbrackish@protonmail.com>
2021-03-11 07:51:00 +0000

    created a setupInput and a handleUserInput

--- client.js
+++ client.js
@@ -1,31 +1,48 @@
 const net = require('net');
-
 const connect = function() {
   const conn = net.createConnection({ 
-    host: '135.23.222.131',
-    port: 50542
+    host: 'localhost',
+    port: 50541
   });    
    conn.on('connect', function() {
       console.log("Successfully connected to game server");
    
-   conn.write('Name: SAM')
-   setInterval(function(){
+   conn.write('Name: SAM'); 
+
+
+
+   setTimeout(function(){
      conn.write("Move: up")
-   }, 100)
-
-
+   }, 100);
+   setInterval(() => {
+     conn.write("Move: right")
+   }, 100);
+   setInterval(() => {
+     conn.write("Move: down")
+   }, 100);
+   setInterval(() => {
+    conn.write("Move: left")
+  }, 100);
   });
-//   conn.on('connect', function() {
-//   conn.write('Move: up');
+ }
+//   
+
+//   // process.stdin.on('data', (key) => {
+//   //   process.stdout.write('.');
+//   // });
+  
+// //   conn.on('connect', function() {
+// //   conn.write('Move: up');
+// // });
+
+//    conn.on('data', (data) => {
+//   console.log(data);
 // });
-
-   conn.on('data', (data) => {
-  console.log(data);
-});
   
-  conn.setEncoding('utf8');
+//   conn.setEncoding('utf8');
   
-  return conn;
-};
+//   return conn;
+// };
 
-module.exports = connect;
+module.exports = connect
+// module.exports = setupInput;
--- play.js
+++ play.js
@@ -1,6 +1,27 @@
 
+
 const connect = require('./client');
 
 console.log('Connecting ...');
 
-connect();
+const handleUserInput = key => {
+  if (key === '\u0003') {
+    console.log("Exit Game");
+    process.exit();
+  }
+  // https://nodejs.dev/learn/how-to-exit-from-a-nodejs-program
+}
+
+
+const setupInput = function(conn) {
+      connection = conn; 
+      const stdin = process.stdin;
+      stdin.setRawMode(true);
+      stdin.setEncoding('utf8');
+      stdin.resume();
+      process.stdin.on("data", handleUserInput);
+      return stdin;
+    };
+
+connect()
+setupInput()
